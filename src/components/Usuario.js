9c668c7b14708cfc7486970c28d962a41a0ef3d7
lgmacedo <lucasguerra.macedo@gmail.com>
2023-02-10 12:29:23 -0300

    added the functionality of changing username and user picture

--- src/components/Usuario.js
+++ src/components/Usuario.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
+
 export default function Usuario(props) {
+    const [nome, setNome] = useState(props.userName);
+    const [foto, setFoto] = useState(props.userPic);
     return (
         <div class="usuario">
-            <img src={props.userPic} alt="imagem de perfil" />
+            <img src={foto} alt="imagem de perfil" onClick={()=>{
+                let novaFoto = prompt("Insira o link da nova image de perfil:");
+                if (novaFoto)
+                    setFoto(novaFoto);
+            }}/>
             <div class="texto">
                 <span>
-                    <strong>{props.userName}</strong>
-                    <ion-icon name="pencil"></ion-icon>
+                    <strong>{nome}</strong>
+                    <ion-icon name="pencil" onClick={() => {
+                        let novoNome = prompt("Insira o novo nome de usuário:");
+                        if (novoNome)
+                            setNome(novoNome);
+                    }}></ion-icon>
                 </span>
             </div>
         </div>
